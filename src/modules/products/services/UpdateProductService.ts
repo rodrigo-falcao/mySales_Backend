c031934bd6292479ee4c30f683d446f8087175a7
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-04 15:42:14 -0300

    Feat:Update product service

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+import { Product } from '../database/entities/Product';
+import { ProductsRepositories } from '../database/repositories/ProductsRepositories';
+
+interface IUpdateProduct {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class CreateProductsService {
+  async execute({
+    id,
+    name,
+    price,
+    quantity
+  }: IUpdateProduct): Promise<Product> {
+
+    const product = await ProductsRepositories.findById(id);
+    if (!product) {
+      throw new AppError('Product not found', 404);
+    }
+
+    const productExists = await ProductsRepositories.findByName(name);
+    if (productExists) {
+      throw new AppError('There is already a product with this name', 409);
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await ProductsRepositories.save(product);
+
+    return product;
+  }
+}
