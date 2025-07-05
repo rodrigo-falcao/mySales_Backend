8de6a292d3be3e73bd612753da70eca7c08bfd28
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-05 09:52:57 -0300

    Feat:Delete Product Service

---
+++ src/modules/products/services/DeleteProductsService.ts
@@ -0,0 +1,19 @@
+import { ProductsRepositories } from '../database/repositories/ProductsRepositories';
+import AppError from '@shared/errors/AppError';
+import { Product } from '../database/entities/Product';
+
+interface IDeleteProduct {
+  id: string;
+}
+
+export default class DeleteProductsService {
+  async execute({ id }: IDeleteProduct): Promise<void> {
+    const product = await ProductsRepositories.findById(id);
+    if (!product) {
+      throw new AppError('Product not found', 404);
+    }
+
+    // Delete the product
+    await ProductsRepositories.remove(product);
+  }
+}
