88ab4ed01256b1b3ad8715651d5b56adf362fbdb
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-08 11:14:27 -0300

    Feat:Create Session User

---
+++ src/modules/users/services/SessionUserService.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { User } from "../database/entities/User";
+import { usersRepository } from "../database/repositories/users.Repositories";
+import AppError from "@shared/errors/AppError";
+
+interface ISessionUser {
+  email: string;
+  password: string;
+}
+
+interface ISessionResponse {
+  user: User;
+  token: string;
+}
+
+export default class SessionUserService {
+  async execute({email, password}: ISessionUser): Promise<ISessionResponse> {
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError("Incorrect email/password combination.", 401);
+    }
+
+    const passwordConfirmation = await compare(password, user.password);
+    if (!passwordConfirmation) {
+      throw new AppError("Incorrect email/password combination.", 401);
+    }
+
+    const token = sign({},process.env.APP_SECRET as string, {
+      subject: String(user.id),
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
