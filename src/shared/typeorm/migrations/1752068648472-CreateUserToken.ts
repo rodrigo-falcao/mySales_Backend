c95f87584a75acba7a3a8d24cd6123962b3ea79a
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-09 11:11:25 -0300

    Feat:Create Migration UserToken

---
+++ src/shared/typeorm/migrations/1752068648472-CreateUserToken.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class  CreateUserToken1752068648472 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+      await queryRunner.createTable(
+        new Table({
+          name: 'user_tokens',
+          columns: [
+            {
+              name: 'id',
+              type: 'integer',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment',
+            },
+            {
+              name: 'token',
+              type: 'uuid',
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'user_id',
+              type: 'integer',
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+          ],
+          foreignKeys: [
+            {
+              name: 'TokenUser',
+              referencedTableName: 'users',
+              referencedColumnNames: ['id'],
+              columnNames: ['user_id'],
+              onDelete: 'CASCADE',
+              onUpdate: 'CASCADE',
+            },
+          ],
+        })
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('user_tokens');
+    }
+
+}
