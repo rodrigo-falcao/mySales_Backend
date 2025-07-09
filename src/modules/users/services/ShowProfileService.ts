4c3c55b954511f0690b88c097d1e9afd60559b09
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-09 15:37:18 -0300

    Feat: Create Show Profile Service

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,19 @@
+import { User } from '../database/entities/User';
+import { usersRepository } from '../database/repositories/UsersRepositories';
+import AppError from '@shared/errors/AppError';
+
+interface IShowProfile {
+  user_id: number;
+}
+
+
+export default class ShowProfileService {
+  async execute({ user_id }: IShowProfile): Promise<User> {
+    const user = await usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    return user;
+  }
+}
