9cb9aa39c6eab26e199685943bc98560ccb11cbc
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-09 12:07:27 -0300

    Feat:Create Forgot and Reset Password Controllers

---
+++ src/modules/users/controllers/ForgotPasswordControllers.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/ServiceForgotPasswordEmailService';
+
+export default class ForgotPasswordControllers {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService();
+
+    await sendForgotPasswordEmailService.execute({ email });
+
+    return response.status(204).json();
+  }
+}
