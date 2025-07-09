cf0d7c1d64c80d6a404e4c91419903eaf66f2c58
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-09 11:35:34 -0300

    Feat:Create Forgot Password Email Service

---
+++ src/modules/users/services/ServiceForgotPasswordEmailService.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { usersRepository } from '../database/repositories/users.Repositories';
+import { usersTokensRepositories } from '../database/repositories/UsersTokensRepositories';
+
+interface IForgotPassword {
+  email: string;
+}
+
+export default class ServiceForgotPasswordEmailService {
+  async execute({ email }: IForgotPassword): Promise<void> {
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('User not found.', 404);
+    }
+
+    const token = await usersTokensRepositories.generate(user.id);
+    console.log(`Token for password reset: ${token}`);
+  }
+
+
+}
