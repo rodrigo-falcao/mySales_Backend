d1836db4dba8f16e7144375f9b06b0b78b2b5bc4
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-12 11:08:46 -0300

    Feat: Create migrations CreateOrders and AddCustomerIdToOders

---
+++ src/shared/typeorm/migrations/1752328574298-CreateOrders.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class  CreateOrders1752328574298 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+          new Table({
+            name: 'orders',
+            columns: [
+              {
+                name: 'id',
+                type: 'integer',
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment',
+              },
+              {
+                name: 'created_at',
+                type: 'timestamp',
+                default: 'now()',
+              },
+              {
+                name: 'updated_at',
+                type: 'timestamp',
+                default: 'now()',
+              },
+            ],
+          })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('orders');
+    }
+
+}
