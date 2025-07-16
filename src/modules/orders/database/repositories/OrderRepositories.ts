a0cd3871317cd85282a67f270f1794f58919bc38
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-16 16:51:53 -0300

    Feat: Create Order Repositories

---
+++ src/modules/orders/database/repositories/OrderRepositories.ts
@@ -0,0 +1,32 @@
+import { AppDataSource } from "@shared/typeorm/data-source";
+import { Order } from "../entities/Order";
+import { OrderProducts } from "../entities/OrderProducts";
+import { Customers } from "@modules/customers/database/entities/Customers";
+
+export const orderProductsRepository = AppDataSource.getRepository(OrderProducts);
+
+interface ICreateOrder {
+  customer: Customers;
+  products: OrderProducts[];
+}
+
+export const orderRepository = AppDataSource.getRepository(Order).extend({
+  async findbyId(id: number): Promise<Order | null> {
+    const order = await this.findOne({
+      where: { id },
+      relations: ["customer", "orders_products"],
+    })
+
+    return order;
+  },
+  async createOrder({ customer, products }: ICreateOrder): Promise<Order> {
+    const order = this.create({
+      customer,
+      orders_products: products,
+    });
+
+    await this.save(order);
+
+    return order;
+  }
+});
