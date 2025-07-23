bdf8f8180ac144a973681da62c5ba24886912aa4
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-23 13:41:20 -0300

    Feat:Install rateLimiter!

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
 import express from 'express';
 import routes from './routes';
+import rateLimiter from '@shared/middlewares/rateLimiter';
 
 AppDataSource.initialize()
   .then(async () => {
@@ -13,6 +14,7 @@ AppDataSource.initialize()
 
     app.use(cors());
     app.use(express.json());
+    app.use(rateLimiter);
 
     app.use(routes);
     app.use(errors());
--- src/shared/middlewares/rateLimiter.ts
+++ src/shared/middlewares/rateLimiter.ts
@@ -1,8 +1,21 @@
 import { RateLimiterRedis } from "rate-limiter-flexible";
+import { Request, Response, NextFunction } from "express";
+import AppError from "../errors/AppError.js";
 
 const limiter = new RateLimiterRedis({
   storeClient: global,
   keyPrefix: "rateLimiter",
   points: 5,
-  duration: 1,
+  duration: 5,
 });
+
+
+export default async function rateLimiter(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    await limiter.consume(req.ip as string);
+
+    return next();
+  } catch (err) {
+    throw new AppError("Too Many Requests", 429);
+  }
+}
